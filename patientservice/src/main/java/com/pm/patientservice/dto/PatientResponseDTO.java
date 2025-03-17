fa1fd6f52ef37b9d693d2261770c3ee166e01429
Utkarsh Gangurde <utkarshgunner10@gmail.com>
2025-03-17 12:16:57 +0530

    Patient module with controller, service, entity, mapper and exception handling

--- patientservice/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
+++ patientservice/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -12,44 +12,39 @@ public class PatientResponseDTO {
         return id;
     }
 
-    public PatientResponseDTO setId(String id) {
+    public void setId(String id) {
         this.id = id;
-        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public PatientResponseDTO setName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public PatientResponseDTO setEmail(String email) {
+    public void setEmail(String email) {
         this.email = email;
-        return this;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public PatientResponseDTO setAddress(String address) {
+    public void setAddress(String address) {
         this.address = address;
-        return this;
     }
 
     public String getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public PatientResponseDTO setDateOfBirth(String dateOfBirth) {
+    public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
-        return this;
     }
 }
